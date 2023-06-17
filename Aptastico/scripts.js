0a203ca90c02d10fb4827855cad9af669f74d3a9
Sergio_Riko <102707805+sergi-2003@users.noreply.github.com>
2023-06-17 00:16:26 -0500

    Add files via upload

---
+++ Aptastico/scripts.js
@@ -0,0 +1,41 @@
+let indice = 1;
+muestraSlides(indice);
+
+function avanzaSlide(n){
+    muestraSlides( indice+=n );
+}
+
+function posicionSlide(n){
+    muestraSlides(indice=n);
+}
+setInterval(function tiempo(){
+    muestraSlides(indice+=1)
+},3000);
+function muestraSlides(n){
+    let i;
+    let slides = document.getElementsByClassName('miSlider');
+    let barras = document.getElementsByClassName('barra');
+
+    if(n > slides.length){
+        indice = 1;
+    }
+    if(n < 1){
+        indice = slides.length;
+    }
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    for(i = 0; i < barras.length; i++){
+        barras[i].className = barras[i].className.replace(" active", "");
+    }
+
+    slides[indice-1].style.display = 'block';
+    barras[indice-1].className += ' active';
+
+}
+
+let btnMenu = document.getElementById('btn-menu');
+let pNav = document.getElementById('principal-nav');
+btnMenu.addEventListener('click', function() {
+  pNav.classList.toggle('mostrar')
+})
